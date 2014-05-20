c4abb717f3793fc6debdc198372f1f0b19d0307f
Srinivas Pradhan <srinivaspradhan64@gmail.com>
2014-05-19 21:34:18 -0400

    Add

--- socket_errors.py
+++ socket_errors.py
@@ -13,7 +13,7 @@ def main():
     given_args = parser.parse_args()
     host = given_args.host
     port = given_args.port
-    filename = given_args.file
+    filename = given_args
     # First try-except block -- create socket
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,9 +21,36 @@ def main():
         print 'Error creating socket: %s' %e
         sys.exit(1)
     
-    # Second try-except block - connect to given host/port
+    # Second try-except block - connect to given \host/port
     try:
         s.connect((host,port))
-        
+    except socket.gaierror, e:
+        print 'Address related error connecting to server: %s' %e
+        sys.exit(1)
+    except socket.error, e:
+        print 'Connection error: %s' %e
+        sys.exit(1)
+    
+    #Third try-except block  --sending data
+    try:
+        s.sendall("GET %s HTTP/1.0\r\n\n\r\n" % filename)
+    except socket.error, e:
+        print 'Error sending data: %s' %e
+        sys.exit(1)
+    # Fourth block waiting on data from the remote host
+    while 1:
+        try:
+            buf = s.recv(2048)
+        except socket.error, e:
+            print 'Error receiving data'
+            sys.exit(1)
+            if not len(buf):
+                break
+            # write the received data
+            sys.stdout.write(buf)
+
+if __name__ =='__main__':
+    main()            
+    
     
     
